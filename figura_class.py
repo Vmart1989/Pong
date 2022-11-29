c0a5e1e4fb4b8060af84ce431642d83b847a421b
Victor Martinez <victor.martinezsegura@gmail.com>
2022-11-29 21:01:27 +0100

    ultimos cambios kata 8

--- figura_class.py
+++ figura_class.py
@@ -12,6 +12,18 @@ class Pelota:
     def dibujar(self,pantalla):
         pg.draw.circle(pantalla,self.color,(self.pos_x, self.pos_y), self.radio)
 
+    def mover(self, y_max=600, x_max=800,y_min=0, x_min=-200):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+
+        if self.pos_y >= y_max-self.radio or self.pos_y <0+self.radio:
+            self.vy *= -1
+
+        if self.pos_x >= x_max+self.radio*12 or self.pos_x < 0-self.radio*12:
+            #contar el gol
+            self.vx *= -1
+            self.vy *= -1
+
 class Raqueta:
     def __init__(self, pos_x,pos_y,w=30,h=100,color=(255,255,255), vx=1, vy=1):
         self.pos_x = pos_x
@@ -25,15 +37,10 @@ class Raqueta:
     def dibujar(self,pantalla):
         pg.draw.rect(pantalla,self.color,(self.pos_x-(self.w//2), self.pos_y-(self.h//2), self.w, self.h))
 
-    def mover(self, pantalla):
+    def mover(self,tecla_arriba,tecla_abajo, y_max=600, y_min=0):
         estado_teclas = pg.key.get_pressed()
 
-        if estado_teclas[pg.K_UP]:
-            self.pos_y -= 0.3
-        if estado_teclas[pg.K_DOWN]:
-            self.pos_y += 0.3
-
-        #if estado_teclas[pg.K_KP8]:
-            #self.pos_y -= 0.3
-        #if estado_teclas[pg.K_KP2]:
-            #self.pos_y += 0.3
+        if estado_teclas[tecla_arriba] and self.pos_y > (y_min + self.h//2):
+            self.pos_y -= self.vy
+        if estado_teclas[tecla_abajo]and self.pos_y < (y_max - self.h//2):
+            self.pos_y += self.vy
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ pg.init()
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption("Pong")
 
+#definir la tasa de refresco de nuestro bucle de fotogramas fps
+cronometro = pg.time.Clock()
+
 pelota = Pelota(400,300)
 raqueta1 = Raqueta(20,300) 
 raqueta2 = Raqueta(780,300)
@@ -13,6 +16,10 @@ raqueta2 = Raqueta(780,300)
 game_over = False
 
 while not game_over:
+    #imprimir los milisegundos que tarda cada fotograma actualmente
+    vt = cronometro.tick(300)#int variable para controlar la velocidad entre fotogramas
+    #print(vt)
+
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
            game_over = True
@@ -24,8 +31,9 @@ while not game_over:
     pg.draw.line(pantalla_principal,(255,255,255), (400,0), (400,600), width=2)
     #pg.draw.lines(pantalla_principal,(255,255,255), False, (p1, p2), width=2)
     pelota.dibujar(pantalla_principal)
+    pelota.mover()
     raqueta1.dibujar(pantalla_principal)
-    raqueta1.mover(pantalla_principal)
     raqueta2.dibujar(pantalla_principal)
-    #raqueta2.mover(pantalla_principal)
+    raqueta1.mover(pg.K_w, pg.K_s)
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
     pg.display.flip()
